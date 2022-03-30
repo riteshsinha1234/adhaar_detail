e58f75106363ca3fa5e836d2bd5347a19108789a
Poonamsh06 <pooamsh061002@gmail.com>
2022-03-30 18:54:42 +0530

    aadhar details ui

--- lib/adhaarView.dart
+++ lib/adhaarView.dart
@@ -1,17 +1,21 @@
+
+
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
 class AdhaarView extends StatelessWidget {
   AdhaarView({Key? key}) : super(key: key);
-  String printText (String text){
+  String printText(String text) {
     print(text);
     return 'hello world';
   }
+
   final myController = TextEditingController();
   @override
-  void dispose(){
+  void dispose() {
     myController.dispose();
   }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -25,9 +29,12 @@ class AdhaarView extends StatelessWidget {
               children: [
                 IconButton(
                   onPressed: () {},
-                  icon: Icon(Icons.arrow_back,color: Colors.black,),
+                  icon: const Icon(
+                    Icons.arrow_back,
+                    color: Colors.black,
+                  ),
                 ),
-                Spacer(),
+                const Spacer(),
                 ElevatedButton(
                   style: ButtonStyle(
                     shape: MaterialStateProperty.all(
@@ -35,16 +42,15 @@ class AdhaarView extends StatelessWidget {
                         borderRadius: BorderRadius.circular(30),
                       ),
                     ),
-                    backgroundColor:
-                    MaterialStateProperty.all(Colors.orange),
+                    backgroundColor: MaterialStateProperty.all(Colors.orange),
                   ),
-                  child: Text(
+                  child: const Text(
                     "Next",
                     style: TextStyle(fontSize: 14, color: Colors.white),
                   ),
                   onPressed: () {},
                 ),
-                SizedBox(
+                const SizedBox(
                   height: 10,
                 ),
               ],
@@ -54,82 +60,136 @@ class AdhaarView extends StatelessWidget {
       ),
       body: Container(
         child: Padding(
-          padding: EdgeInsets.fromLTRB(20, 30, 20, 10),
+          padding: const EdgeInsets.fromLTRB(20, 20, 20, 10),
           child: SingleChildScrollView(
-            child: Column(
-              children: [
-                SizedBox(
-                  child: Text(
-                    "Adhaar Details",
-                    style: TextStyle(
-                        color: Colors.black,
-                        fontWeight: FontWeight.bold,
-                        fontSize: 20),
+            child: Expanded(
+              child: Column(
+                children: [
+                  const SizedBox(
+                    child: Text(
+                      "Adhaar Details",
+                      style: TextStyle(
+                          color: Colors.black,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 20),
+                    ),
                   ),
-                ),
-                SizedBox(
-                  height: 20,
-                ),
-                TextField(
-                  decoration: InputDecoration(
-                    border: OutlineInputBorder(
-                      borderSide: BorderSide(color: Colors.orange,width: 2.0),
-                    ),
-                    enabledBorder: OutlineInputBorder(
-                      borderSide: BorderSide(color: Colors.orange, width: 2.0)
-                    ),
-                    labelText: "Your name as per Adhaar",
-                    labelStyle: TextStyle(color: Colors.grey),
-                    contentPadding: EdgeInsets.all(40),
+                  const SizedBox(
+                    height: 20,
                   ),
-                ),
-                SizedBox(
-                  height: 20,
-                ),
-                TextField(
-                  decoration: InputDecoration(
-                    border: OutlineInputBorder(
-                      borderSide: BorderSide(color: Colors.orange,width: 2.0),
+                  const TextField(
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.orange, width: 1.0),
+                      ),
+                      enabledBorder: OutlineInputBorder(
+                          borderSide:
+                              BorderSide(color: Colors.orange, width: 1.0)),
+                      labelText: "Your name as per Adhaar",
+                      labelStyle: TextStyle(color: Colors.grey),
+                      contentPadding: EdgeInsets.all(30),
                     ),
-                    enabledBorder: OutlineInputBorder(
-                        borderSide: BorderSide(color: Colors.orange, width: 2.0)
-                    ),
-                    labelText: "Permanent address",
-                    labelStyle: TextStyle(color: Colors.grey),
-                    contentPadding: EdgeInsets.all(40),
                   ),
-                ),
-                SizedBox(
-                  height: 20,
-                ),
-                TextField(
-                  obscureText: false,
-                  controller: myController,
-                  maxLength: 16,
-                  keyboardType: TextInputType.number,
-                  inputFormatters: <TextInputFormatter>[
-                    FilteringTextInputFormatter.digitsOnly,
-                  ],
-                  decoration: InputDecoration(
-                    border: OutlineInputBorder(
-                      borderSide: BorderSide(color: Colors.orange,width: 2.0),
-                    ),
-                    enabledBorder: OutlineInputBorder(
-                        borderSide: BorderSide(color: Colors.orange, width: 2.0)
-                    ),
-                    labelText: "Adhaar number",
-                    labelStyle: TextStyle(color: Colors.grey),
-                    contentPadding: EdgeInsets.all(40),
+                  const SizedBox(
+                    height: 20,
                   ),
-                ),
-                SizedBox(
-                  height: 20,
-                ),
-              ],
+                  const TextField(
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.orange, width: 1.0),
+                      ),
+                      enabledBorder: OutlineInputBorder(
+                          borderSide:
+                              BorderSide(color: Colors.orange, width: 1.0)),
+                      labelText: "Permanent address",
+                      labelStyle: TextStyle(color: Colors.grey),
+                      contentPadding: EdgeInsets.all(30),
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  TextField(
+                    obscureText: false,
+                    controller: myController,
+                    maxLength: 16,
+                    keyboardType: TextInputType.number,
+                    inputFormatters: <TextInputFormatter>[
+                      FilteringTextInputFormatter.digitsOnly,
+                    ],
+                    decoration: const InputDecoration(
+                      border: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.orange, width: 1.0),
+                      ),
+                      enabledBorder: OutlineInputBorder(
+                          borderSide:
+                              BorderSide(color: Colors.orange, width: 1.0)),
+                      labelText: "Adhaar number",
+                      labelStyle: TextStyle(color: Colors.grey),
+                      contentPadding: EdgeInsets.all(40),
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  containerWidget('Please upload your adhaar(front side)'),
+                 const SizedBox(height: 8,),
+                  containerWidget('Please upload your adhaar(back side)'),
+                ],
+              ),
             ),
           ),
         ),
       ),
     );
   }
+}
+
+Widget containerWidget(text) {
+  return Container(
+    width: 400,
+    height: 300,
+    decoration: BoxDecoration(
+      borderRadius: BorderRadius.circular(8.0),
+      border: Border.all(color: Colors.orange, width: 1.0),
+    ),
+    child: Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Column(children: [
+        Text(
+          text,
+          style: const TextStyle(fontSize: 12, color: Colors.black),
+        ),
+        const SizedBox(
+          height: 10,
+        ),
+        Container(
+          width: 330,
+          height: 200,
+         
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(6.0),
+             color: Colors.grey,
+
+          ),
+          child: const Center(
+            child:  Text("Add Image",style: TextStyle(fontSize: 12, color: Colors.white),
+),
+          ),
+        ),
+       const SizedBox(height: 5,),
+       Padding(
+         padding: const EdgeInsets.only(top: 5,right: 10,left: 10,bottom: 7),
+         child: Row(
+           children:const [
+              Icon(Icons.insert_photo_rounded,color: Colors.red,size: 27,),
+              SizedBox(width: 10,),
+              Icon(Icons.camera_alt_rounded,color: Colors.red,size: 27,),
+
+           ],
+         ),
+       )
+      ]),
+    ),
+  );
 }
